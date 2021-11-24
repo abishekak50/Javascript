89745d9589506907bc2bca8e33b9247f3de38a9e
abishekak50 <abishekajiabi20@gmail.com>
2021-11-24 09:26:43 +0530

    create javascript project

---
+++ src/org/javascript/Sri.java
@@ -0,0 +1,9 @@
+package org.javascript;
+
+public class Sri {
+
+	public static void main(String[] args) {
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sridhar\\eclipse-workspace\\Text\\driver\\chromedriver.exe");
+	}
+
+}
